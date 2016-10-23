11bd291f78cd9c1eeb50e04d6d6c59048f0b4b0b
Mikołaj Chwalisz <m.chwalisz@gmail.com>
2016-10-23 21:12:41 +0200

    configure docs

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     install_requires=['requests', 'docopt'],
 
     extras_require={
-        'dev': ['check-manifest', 'tox', 'sphinx'],
+        'dev': ['check-manifest', 'tox', 'sphinx', 'sphinx_bootstrap_theme'],
         'test': ['coverage'],
     },
     entry_points={
